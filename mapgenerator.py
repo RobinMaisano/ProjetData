b652e5fbf7787093cafccd8f8d5c4d66f16fe616
zelra <pierre.gilly@viacesi.fr>
2019-07-02 17:08:28 +0200

    Added generated maps

--- mapgenerator.py
+++ mapgenerator.py
@@ -1,18 +1,17 @@
 import random
 import csv
-import numpy
 
 def randGen(maxRange):
     return (random.randint(0, maxRange))
 
-def mapGenerator(citiesNumber, maxDistance):
+def mapGenerator(citiesNumber, maxDistance, filename):
     x = []
     y = []
     for i in range(citiesNumber):
         x.append(randGen(maxDistance))
         y.append(randGen(maxDistance))
     print(x, "\n", y)
-    f = open("map.csv", 'w')
+    f = open(filename, 'w')
     for i, j in zip(x, y):
         f.write(str(i) + "," + str(j) + "\n")
     f.close()
@@ -30,7 +29,27 @@ def readMap(filename):
             y.append(int(yRow))
 
         print(x, "\n", y)
+        return x,y
+'''
+class City:
+    def __init__(self, x=0, y=0, name="null"):
+        self.x = x
+        self.y = y
+        self.name = name
 
+def listToMatrix(x, y):
+    matrixLength = len(x)
+    print("Le longueur est : ", matrixLength, "\n\n")
+    matrix = []
+    citiesList = []
+    for i in range(matrixLength-1):
+        matrix.append([0] * matrixLength)
+#        print(matrix[i])
+    for j in range(len(x)):
+#        print("\nx : ", x[j], "y : ", y[j])
+        citiesList.append(City(x[j], y[j], j))
+        print(citiesList[j].name)'''
 
-#mapGenerator(1000, 1000)
-readMap("map.csv")
+mapGenerator(200, 500, "maps/map02.csv")
+x, y = readMap("maps/map1.csv")
+#listToMatrix(x, y)
