b84d3fd11b9e1a56a85c5905f716b073769b955d
Robin Maisano <robin.maisano@gmail.com>
2019-07-02 17:04:33 +0200

    Annealing Begins - Paused to complete project minimas

--- simulatedAnnealing.py
+++ simulatedAnnealing.py
@@ -31,6 +31,12 @@ beginningTime = time.process_time()
 # Variables
 
 N_trucks = 2
+N_cities = 15
+Temp = 20
+TempMin = 5
+Energie = 0
+Coeff = 0.99
+
 cities = []
 x = [10, 83, 64, 44, 8, 34, 86, 12, 70, 87, 52, 24, 29, 66, 40]
 y = [34, 90, 43, 72, 71, 28, 20, 10, 70, 54, 90, 93, 48, 16, 10]
@@ -38,18 +44,26 @@ y = [34, 90, 43, 72, 71, 28, 20, 10, 70, 54, 90, 93, 48, 16, 10]
 for i in range(len(x)):
     cities.append(City(x[i], y[i]))
 
-array1 = np.array_split(cities, N_trucks)
+clusters = np.array_split(cities, N_trucks)
 i = 0
-print(array1)
-for item in array1:
+for cluster in clusters:
     i += 1
-    print(i)
-    for city in item:
+    print("\n" + str(i))
+    for city in cluster:
         print(str(city.x) + ", " + str(city.y))
+
+while (Temp > TempMin):
+        # Cluster selection (random)
+        iCluster = np.random.randint(high=len(clusters))
+        if len(clusters[iCluster]) == 0:
+                continue
+        
+        # Index selection (in cluster)
+        indexCity = np.random.randint(high=len(cluster))
 
 endTime = time.process_time()
 totalTime = endTime - beginningTime
 
-print(beginningTime)
-print(endTime)
-print(totalTime)
+print("\nBeginning Time: " + str(beginningTime))
+print("End Time:       " + str(endTime))
+print("Total Time:     " + str(totalTime))
