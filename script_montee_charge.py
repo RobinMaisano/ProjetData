3e221bfd250d7592d5c145d8b7519723eab099d3
zelra <pierre.gilly@viacesi.fr>
2019-07-03 16:02:18 +0200

    Added time at the end of the script

--- script_montee_charge.py
+++ script_montee_charge.py
@@ -14,3 +14,19 @@ if len(sys.argv) == 3:
 for files in os.listdir(map_directory):
     print("Currently: " + str(files))
     subprocess.run(["python", "script_cluster_greedy.py", str(map_directory) + "\\" + str(files), n_trucks])
+
+
+def final_time(truck_number):  # Calculate the time required for maps
+    time_counter = 0
+    buffer = 0
+    file = reversed(list(open("stats.csv")))  # Read the file from end to the top
+    for line in file:
+        buffer = buffer + 1
+        if buffer < int(truck_number):  # For each line
+            parsed = line.rstrip().split(",")  # Parse the time
+            time_counter = time_counter + float(parsed[1])  # Add the time to the finalTime
+    print("\nFinal time to calculate all the maps : " + str(time_counter) + " (in seconds)")
+
+
+final_time(sys.argv[2])
+
