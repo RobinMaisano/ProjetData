e35d3a90d71733f8de788e5c55437a62c830f13a
zelra <pierre.gilly@viacesi.fr>
2019-07-03 20:43:02 +0200

    Converted code to PEP8 format

--- mapgenerator.py
+++ mapgenerator.py
@@ -1,35 +1,41 @@
 import random
 import csv
 
-def randGen(maxRange):
-    return (random.randint(0, maxRange))
 
-def mapGenerator(citiesNumber, maxDistance, filename):
+# Generate random number
+def rand_gen(max_range):
+    return random.randint(0, max_range)
+
+
+# Generate random list of points and put them in the csv file
+def map_generator(cities_number, max_distance, filename):
     x = []
     y = []
-    for i in range(citiesNumber):
-        x.append(randGen(maxDistance))
-        y.append(randGen(maxDistance))
+    for i in range(cities_number):  # Generate x et y lists with random numbers
+        x.append(rand_gen(max_distance))
+        y.append(rand_gen(max_distance))
     print(x, "\n", y)
-    f = open(filename, 'w')
-    for i, j in zip(x, y):
-        f.write(str(i) + "," + str(j) + "\n")
+    f = open(filename, 'w')  # Open csv file
+    for j, k in zip(x, y):  # For each element in lists, put them in the csv file
+        f.write(str(j) + "," + str(k) + "\n")
     f.close()
 
-def readMap(filename):
-    with open(filename) as csvfile:
-        readCSV = csv.reader(csvfile, delimiter=',')
+
+# Read th csv file
+def read_map(filename):
+    with open(filename) as csv_file:
+        read_csv = csv.reader(csv_file, delimiter=',')
         x = []
         y = []
-        for row in readCSV:
-            xRow = row[0]
-            yRow = row[1]
+        for row in read_csv:
+            x_row = row[0]
+            y_row = row[1]
 
-            x.append(int(xRow))
-            y.append(int(yRow))
+            x.append(int(x_row))
+            y.append(int(y_row))
 
         print(x, "\n", y)
-        return x,y
+        return x, y
 '''
 class City:
     def __init__(self, x=0, y=0, name="null"):
@@ -53,6 +59,6 @@ def listToMatrix(x, y):
 cities = 0
 for i in range(50):
     cities = cities + 100
-    mapGenerator(cities, 2000, "maps/map"+ str(cities) +".csv")
-#x, y = readMap("maps/map1.csv")
-#listToMatrix(x, y)
+    map_generator(cities, 2000, "maps/map" + str(cities) + ".csv")
+# x, y = read_map("maps/map1.csv")
+# listToMatrix(x, y)
